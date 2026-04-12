a39abb4651ca55ab28d61a63b0e4cb327bd094fd
BryanSalgado440 <bsalgado440@gmail.com>
2026-04-11 21:35:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ function typeWriter(element, htmlContent, container) {
             element.innerHTML = htmlContent;
             clearInterval(typingInterval);
         }
-    }, 45); 
+    }, 70); 
 }
 
 // Para la "Carta Para Ti" (Navegación entre textos originales)
